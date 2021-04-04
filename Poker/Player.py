f844560dc2fda6f272a085025458f8c7e55fe07f
Esteban Acosta <estebanacosta19@gmail.com>
2021-04-04 18:21:48 -0400

    setting up poker game

---
+++ Poker/Player.py
@@ -0,0 +1,79 @@
+class Player:
+    def __init__(self,id):
+        self.setPlayerID(id)
+        self.setPlayerHand([])
+        self.setName("")
+        self.setCurrentScore(0)
+
+    def setPlayerID(self,id):
+        self.__id = id
+
+    def getPlayerID(self):
+        return self.__id
+
+    def getName(self):
+        return self.__name
+
+    def setName(self,name):
+        self.__name = name
+
+    def getPlayerHand(self):
+        return self.__playerHand
+
+    def setPlayerHand(self,playerHand):
+        self.__playerHand = playerHand
+
+    def setCurrentScore(self, score):
+        self.__current_score = 0
+
+    def getCurrentScore(self):
+        return self.__current_score
+
+    def addToCurrentScore(self, score):
+        self.__current_score += score
+
+    def getPlayerHandSize(self):
+        return len(self.__playerHand)
+
+    def addMultipleToPlayerHand(self,moreCards):
+        self.__playerHand.extend(moreCards)
+
+    def addOneToPlayerHand(self,oneCard):
+        self.__playerHand.append(oneCard)
+
+    def removeOneFromPlayerHand(self,position):
+        return self.__playerHand.pop(position - 1)
+
+    def removeMultipleFromPlayerHand(self,newCards):
+        for card in newCards:
+            for playerCard in self.__playerHand:
+                if card == playerCard:
+                    self.__playerHand.remove(playerCard)
+
+    def clearHand(self):
+        self.__playerHand.clear()
+
+    def containsThisCard(self,thisCard):
+        for eachCard in self.__playerHand:
+            if eachCard == thisCard:
+                return True
+        return False
+
+    def getCard(self,position):
+        return self.__playerHand[position - 1]
+
+    def hasMatchingCard(self,topCard):
+        for card in self.__playerHand:
+            if card.getSuit() == topCard.getSuit() or card.getRank() == topCard.getRank() or card.getRank() == "EIGHT":
+                return True
+        return False
+
+    def showPlayerCards(self):
+        print(self.getName() + "'s Hand: ")
+        position = 1
+        for card in self.getPlayerHand():
+            print(str(position)+ ": " + "( "  + card.__str__() + " )")
+            position+=1
+        print()
+
+
---
+++ Poker/Poker.py
@@ -0,0 +1,83 @@
+from Suits import*
+from Ranks import*
+from Card import*
+from Player import*
+from Deck import*
+import re
+import random
+
+players = []
+
+def addPlayers(howManyPlayers):
+    for numPlayer in range(howManyPlayers):
+        players.append(Player(numPlayer + 1))
+
+def setUpNumPlayers():
+    inputNumPlayers = ""
+    numPlayers = 0
+    minPlayers = 2
+    maxPlayers = 4
+
+    print("Welcome to Poker\n")
+    print("How many players do you want in this game?")
+    print("Minimum number of players is " + str(minPlayers) + " and maximum number of players is " + str(maxPlayers) +"\n")
+
+    #continue prompting the player until they enter a number that is within the number of players allowed in the game
+    while numPlayers < minPlayers or numPlayers > maxPlayers:
+        inputNumPlayers =input("Please keep the number of players between " + str(minPlayers) + " and " + str(maxPlayers) + "\n")
+
+        #prompt player again if their input isn't a number
+        while re.match("[0-9]+$",inputNumPlayers) == None:
+           inputNumPlayers = input("Please enter a whole number\n")
+
+        #convert string input into an integer
+        numPlayers = int(inputNumPlayers)
+
+    addPlayers(numPlayers)
+
+    print(str(numPlayers) + " players have been added to the game\n")
+
+    print("_____________________________________________________________")
+
+def setUpPlayers():
+    #set up the deck
+    deck = Deck()
+    #shuffle the deck
+    deck.shuffle()
+    #var stores how many cards to deal to each player
+    howManyCardsEachPlayerNeeds = 5
+    #set a character limit
+    limit = 10
+
+    #loop through the players
+    for i in range(len(players)):
+        #draw multiple cards from the deck and add it to the player's hand
+        players[i].setPlayerHand(deck.drawMultiple(howManyCardsEachPlayerNeeds))
+        #get player input
+        name = input("Please enter a name\n")
+        #Until the player puts a name that is within the limit and doesn't have a number in it
+        while len(name) > limit or any(char.isdigit() for char in name) == True:
+            errorMessage = ""
+            #print the error message
+            if len(name) > limit :
+                errorMessage = "Please enter a name that has less than " + str(limit) + " characters\n"
+            else:
+                errorMessage = "Please enter a name with no numbers in it\n"
+            # continue prompting them
+            name = input(errorMessage)
+
+        players[i].setName(name)
+
+        print(players[i].getName() + " has " + str(players[i].getPlayerHandSize()) + " cards")
+
+        print("___________________________________________________________________________")
+
+def determineRanking(hand):
+
+    for card in hand:
+        pass
+#main method
+def run():
+    setUpNumPlayers()
+    setUpPlayers()
+run()
